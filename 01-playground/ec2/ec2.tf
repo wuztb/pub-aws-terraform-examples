53bcd1ab3df7c715f62c04e4c00514cebe6acb87
wuztb <bruckmayer@gmail.com>
2023-01-24 16:22:38 +0100

    minor updates

--- 01-playground/ec2/ec2.tf
+++ 01-playground/ec2/ec2.tf
@@ -23,6 +23,8 @@ variable "amis" {
 resource "aws_instance" "example" {
     ami           = var.amis[var.region].ami
     instance_type = "t2.micro"
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = var.vpc_security_group_ids
     tags = {
         Name = var.name
     }
--- 01-playground/ec2/vars.tf
+++ 01-playground/ec2/vars.tf
@@ -7,3 +7,13 @@ variable "name" {
   type        = string
   description = "The name"
 }
+
+variable "subnet_id" {
+  type        = string
+  description = "The subnet id"
+}
+
+variable "vpc_security_group_ids" {
+  type        = list(string)
+  description = "The security group ids"
+}
